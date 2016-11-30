f61d17e2ea81c6b9164fd307f4d673706df638cc
Dorothy <jhu4@wpi.edu>
2016-11-30 12:21:27 -0500

    working on Hu_StealHealthGoal_Evaluator::ClosestActiveHealth

--- Raven/Bots/HuBot/Hu_StealHealthGoal_Evaluator.cpp
+++ Raven/Bots/HuBot/Hu_StealHealthGoal_Evaluator.cpp
@@ -1,8 +1,17 @@
 #include "Hu_StealHealthGoal_Evaluator.h"
+#include "HuGoal_Think.h"
+
 #include "misc/Stream_Utility_Functions.h"
 #include "../../Raven_ObjectEnumerations.h"
 #include "../../goals/Raven_Feature.h"
-#include "HuGoal_Think.h"
+#include "../../Raven_SensoryMemory.h"
+#include "../../Raven_Game.h"
+#include "../../Raven_Map.h"
+#include "../../triggers/Trigger_HealthGiver.h"
+#include "2D\Vector2D.h"
+#include "Triggers\TriggerSystem.h"
+#include "../../triggers/TriggerSystem.h"
+
 
 double Hu_StealHealthGoal_Evaluator::CalculateDesirability(AbstRaven_Bot* pBot) {
 	//first grab the distance to the closest instance of a health item
@@ -18,7 +27,7 @@ double Hu_StealHealthGoal_Evaluator::CalculateDesirability(AbstRaven_Bot* pBot) 
 
 
 	double dist_to_bot = Raven_Feature::DistanceToItem(pBot, type_bot);
-
+	
 
 
 }
@@ -29,5 +38,44 @@ void  Hu_StealHealthGoal_Evaluator::SetGoal(AbstRaven_Bot* pEnt) {
 
 void Hu_StealHealthGoal_Evaluator::RenderInfo(Vector2D Position, AbstRaven_Bot* pBot) {
 
+}
+
+
+
+Trigger<AbstRaven_Bot>* Hu_StealHealthGoal_Evaluator::ClosestActiveHealth(AbstRaven_Bot* opponent) {
+	
+	//iterate throught all of the trigger in the map
+	Raven_Map::TriggerSystem::TriggerList trigger_list = opponent->GetWorld()->GetMap()->GetTriggers();
+	Raven_Map::TriggerSystem::TriggerList::const_iterator it;
+	for (it = trigger_list.begin(); it != trigger_list.end(); it++) {
+		//only if the trigger is a health trigger
+		if ((*it)->EntityType() == type_health) {
+
+		}
+	}
+
+
+}
+
+AbstRaven_Bot* Hu_StealHealthGoal_Evaluator::ClosestBotToHealth(AbstRaven_Bot* mybot, Trigger<AbstRaven_Bot>* health_pack){
+	std::list<AbstRaven_Bot*> opponent_list = mybot->GetSensoryMem()->GetListOfRecentlySensedOpponents();
+
+	double smallest_dist = MaxDouble;
+	AbstRaven_Bot* closest_bot = NULL;
+
+
+	//iterate throught the opponent bot list and find out the closest bot to the health pack
+	std::list<AbstRaven_Bot*>::const_iterator it;
+	for (it = opponent_list.begin(); it != opponent_list.end(); it++) {
+		
+		double dist = Vec2DDistanceSq((*it)->Pos(), health_pack->Pos());
+
+		if (dist <= smallest_dist) {
+			closest_bot = (*it);
+			smallest_dist = dist;
+		}
+	}	
+
+	return closest_bot;
 }
 
--- Raven/Bots/HuBot/Hu_StealHealthGoal_Evaluator.h
+++ Raven/Bots/HuBot/Hu_StealHealthGoal_Evaluator.h
@@ -4,6 +4,8 @@
 
 #include "../../AbstRaven_Bot.h"
 #include "../../goals/Goal_Evaluator.h"
+#include "../../triggers/Trigger.h"
+
 
 class Hu_StealHealthGoal_Evaluator : public Goal_Evaluator
 {
@@ -16,6 +18,13 @@ public:
 	void  SetGoal(AbstRaven_Bot* pEnt);
 
 	void RenderInfo(Vector2D Position, AbstRaven_Bot* pBot);
+
+	//helper function
+	//Given a bot, return the pointer to the closest health pack
+	Trigger<AbstRaven_Bot>* ClosestActiveHealth(AbstRaven_Bot* opponent);
+	//Given my bot and a pointer to a health pack, return the closest opponent to the health pack
+	AbstRaven_Bot* ClosestBotToHealth(AbstRaven_Bot* mybot, Trigger<AbstRaven_Bot>* health_pack);
+
 };
 
 
